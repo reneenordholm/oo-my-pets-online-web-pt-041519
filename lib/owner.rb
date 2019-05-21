4aabbfc791010152c2d2cfeeb7a0ee9d359083a3
Renee Nordholm <reneenordholm@gmail.com>
2019-05-21 20:30:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,13 +2,19 @@ class Owner
   
 
   @@all = []
+  @@count = []
   
   def initialize(owner)
     @owner = owner
     @@all << self
+    @@count << self
   end
 
   def self.all
     @@all
+  end
+  
+  def self.count
+    @@count.count
   end
 end
