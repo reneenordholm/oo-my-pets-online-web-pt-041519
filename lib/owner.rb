e3d4eb3e6bd6e0bc7da03bbe6c7d2bffb2d55aae
Renee Nordholm <reneenordholm@gmail.com>
2019-05-21 22:30:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,6 +43,12 @@ class Owner
       cat.mood = 'happy'
     end
   end
+  
+ def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = 'happy'
+    end
+  end
 
   def self.all
     @@all
