214fc7c3712fcef8f83e6a716f57444f949b091a
Renee Nordholm <reneenordholm@gmail.com>
2019-05-21 20:06:09 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -6,11 +6,4 @@ class Cat
     @name = name
   end
   
-  def owner=(owner)
-    #if owner == nil then we can set the owner
-    #else we cannot set the owner
-      if @owner == nil 
-        @owner = owner
-      end
-    end
 end
